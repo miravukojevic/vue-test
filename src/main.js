968a08f73cb7f2c666e25eb3f012a5cfd1929ada
Mira Vukojevic <miraperovic034@gmail.com>
2018-03-18 23:01:43 +0100

    vuetable

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'bootstrap'
+import Vuetable from 'vuetable-2'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+Vue.use(Vuetable);
 
 /* eslint-disable no-new */
 new Vue({
