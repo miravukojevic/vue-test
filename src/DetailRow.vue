7c3edd78631a3f1015c79f78e28a854a97725551
Mira Vukojevic <miraperovic034@gmail.com>
2018-03-20 23:26:17 +0100

    detail row 2

--- src/DetailRow.vue
+++ src/DetailRow.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
+    <div class="details-page">
       <h1>Welcome to detail page</h1>
-      <p> aircraft's model.{{activeDetail.Mdl}}</p>
-      <p> The manufacturer's name {{activeDetail.Man}}</p>
-      <p> The code and name of the departure airport {{activeDetail.From}}</p>
-      <p>   The code and name of the arrival airport {{activeDetail.To}}</p>
-      <img src="//logo.clearbit.com/koreanair.com">
+      <p> aircraft's model: {{activeDetail.Mdl}}</p>
+      <p> The manufacturer's name: {{activeDetail.Man}}</p>
+      <p> The code and name of the departure airport: {{activeDetail.From}}</p>
+      <p>   The code and name of the arrival airport: {{activeDetail.To}}</p>
+      <p>Op je:{{activeDetail.Op}}</p>
+      <img v-if="" :src="'//logo.clearbit.com/' + urlImg + '.com'">
+      <a @click="$router.go(-1)">Back to aircraft list</a>
       <!-- <router-link :to="-1">back</router-link> -->
     </div>
 </template>
@@ -17,45 +19,25 @@
       return{
         airInfo:'',
         apiurl: 'http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?fId=11315604',
-        detailtest: ''
+        urlImg: ''
       }
     },
     created(){
+      let test = this.activeDetail.Op
+      this.urlImg = test.split(' ').slice(0,2).join('').toLowerCase();
 
-      this.detailsRow = this.$store.getters.getDetails;
-            console.log(this.detailsRow);
-      // this.$http.jsonp(this.apiurl).then(response => {
-
-
-      //     // get status
-      //     response.status;
-
-      //     // get status text
-      //     response.statusText;
-
-      //     // get 'Expires' header
-      //     response.headers.get('Expires');
-
-      //     // get body data
-
-      //     this.airInfo = response.data.acList
-      //       console.log('tu sam');
-
-      //     // setTimeout(this.fetchData(), 200);
-      //     this.showLoad = false;
-
-      //     }, response => {
-      //       // error callback
-      //     });
+      console.log(this.urlImg)
     },
     computed: {
       ...mapGetters(['activeDetail'])
     },
     methods: {
-      onClick (event) {
-        console.log('my-detail-row: on-click', event.target)
-      }
+
     },
   }
   </script>
-//  http://andreybleme.com/2018-01-07/sharing-data-across-vuejs-components
+<style>
+.details-page{
+color: #fff
+}
+</style>
